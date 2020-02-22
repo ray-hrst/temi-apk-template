300e066065e6faeb824466a0b06c8eecc706607b
Raymond Oung <r.oung@hapi-robo.com>
2020-02-22 16:40:55 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# temi APK Template
+A template project used to create temi apps.
---
+++ app/src/main/java/com/hapirobo/shortcut_template/MainActivity.java
@@ -0,0 +1,30 @@
+package com.hapirobo.shortcut_template;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String package_name = getResources().getString(R.string.package_name);
+        launchApp(package_name);
+    }
+
+    protected void launchApp(String packageName) {
+        Intent mIntent = getPackageManager().getLaunchIntentForPackage(packageName);
+
+        if (mIntent != null) {
+            try {
+                startActivity(mIntent);
+            } catch (ActivityNotFoundException err) {
+                Toast t = Toast.makeText(getApplicationContext(), R.string.app_not_found, Toast.LENGTH_SHORT);
+                t.show();
+            }
+        }
+    }
+}
